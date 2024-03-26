7bc8b1fbc16183fb9d9d0702cef2f9dab2846c8d
NaZuL Garrido <nazul.garrido@vantis.mx>
2024-03-25 19:52:28 -0600

    test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send("Hello Mundo!"));
+app.get("/", (req, res) => res.send("Hello Nazul!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
